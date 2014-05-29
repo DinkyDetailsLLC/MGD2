e426caad57da54c506ea3d22f3317f4d92a598fd
Dan <Dan@Dinkydetails.com>
2014-05-28 22:33:03 -0400

    Adding more Scenes and Titles and images..

---
+++ SpriteKit/SpriteKit/SpriteKit/CreditScene.h
@@ -0,0 +1,13 @@
+//
+//  CreditScene.h
+//  SpriteKit
+//
+//  Created by DANIEL ANNIS on 5/28/14.
+//  Copyright (c) 2014 Dinky_Details. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface CreditScene : SKScene
+
+@end
---
+++ SpriteKit/SpriteKit/SpriteKit/CreditScene.m
@@ -0,0 +1,42 @@
+//
+//  CreditScene.m
+//  SpriteKit
+//
+//  Created by DANIEL ANNIS on 5/28/14.
+//  Copyright (c) 2014 Dinky_Details. All rights reserved.
+//
+
+#import "CreditScene.h"
+
+@implementation CreditScene
+
+-(id) initWithSize:(CGSize)size{
+    if (self = [super initWithSize:size]){
+        //setup your scene
+        
+        [self createBackground];
+        
+    }return self;
+}
+
+//create backgeround
+-(void) createBackground{
+    SKTexture *bgTexture = [SKTexture textureWithImageNamed:@"commonBG"];
+    self.background = [SKSpriteNode spriteNodeWithTexture:bgTexture];
+    self.background.position = (CGPoint) {CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)};
+    [self addChild:self.background];
+}
+
+-(void) createTitleAndText{
+    self.title = [SKLabellNode labelNodeWithFontNamed:@"hobo"];
+    self.title.text =@"Credits";
+    self.title.fontSize = 30;
+    self.title.fontColor = [SKColor blackColor];
+    self.title.position = (CGPoint) {CGRectGetMidX(self.frame),CGRectGetMidY(self.frame)-50};
+    [self.addChild:self.title];
+    
+    }
+
+-(void) createBackButton{
+}
+@end
